96beea20704d92208c37cc1f0f4b1adb0bcf9e03
Robert deLaguna <rjdelaguna@yahoo.com>
2023-06-01 10:19:00 -0700

    Complete YOU DO #2

--- comparisons.rb
+++ comparisons.rb
@@ -26,7 +26,7 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 puts "Is number_teachers less than number_students?", number_teachers < number_students
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
 
