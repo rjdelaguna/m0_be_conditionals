1c68def537f5ef86badd12e1bdd52cd469f32984
Robert deLaguna <rjdelaguna@yahoo.com>
2023-06-01 12:22:06 -0700

    Correct Spicy Challenge

--- code_challenges/spicy_challenge.rb
+++ code_challenges/spicy_challenge.rb
@@ -1,10 +1,12 @@
 # Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer. If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.
 
-value = 45
+value = 41
 if (value%3) == 0 && (value%5) == 0
     puts "FizzBuzz"
 elsif (value%3) == 0
     puts "Fizz"
-else (value%5) == 0
+elsif (value%5) == 0
     puts "Buzz"
+else
+    puts value
 end
